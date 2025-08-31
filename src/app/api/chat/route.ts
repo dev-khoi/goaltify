2aaecea002c0b198ffcec10c336c417ec0ba4cd9
dev-khoi <daknguyen111@gmail.com>
2025-08-31 17:35:45 -0400

    created basic chatbot interaction

---
+++ src/app/api/chat/route.ts
@@ -0,0 +1,37 @@
+import { google } from "@ai-sdk/google";
+import {
+  streamText,
+  UIMessage,
+  convertToModelMessages,
+  tool,
+  stepCountIs,
+} from "ai";
+import { z } from "zod";
+// Allow streaming responses up to 30 seconds
+export const maxDuration = 30;
+
+export async function POST(req: Request) {
+  const { messages }: { messages: UIMessage[] } = await req.json();
+
+  const result = streamText({
+    model: google("gemini-2.5-pro"),
+    messages: convertToModelMessages(messages),
+    stopWhen: stepCountIs(5),
+    // tools: {
+    //   weather: tool({
+    //     description: 'Get the weather in a location (fahrenheit)',
+    //     inputSchema: z.object({
+    //       location: z.string().describe('The location to get the weather for'),
+    //     }),
+    //     execute: async ({ location }) => {
+    //       const temperature = Math.round(Math.random() * (90 - 32) + 32);
+    //       return {
+    //         location,
+    //         temperature,
+    //       };
+    //     },
+    //   }),
+    // },
+  });
+  return result.toUIMessageStreamResponse();
+}
